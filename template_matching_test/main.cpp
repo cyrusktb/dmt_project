61d9056f047c25d35ff96e7154e32288e2751a17
Nick Hafner <nrh16@ic.ac.uk>
2019-04-17 18:25:28 +0100

    Tried out new template matching methods

--- template_matching_test/main.cpp
+++ template_matching_test/main.cpp
@@ -15,30 +15,63 @@ int main(int argc, char **argv) {
 
     // Read in the template images
     cv::Mat templates[3];
-    for(char i = 0; i < 3; i++)
+    for(char i = 0; i < 3; i++) {
         templates[i] = cv::imread(argv[i+1], 0);
+        cv::resize(templates[i], templates[i], cv::Size(0,0), 0.5, 0.5);
+    }
 
     // Read in the actual image
     cv::Mat displayFrame = cv::imread(argv[4], 0);
 
     cv::Mat template_match_res[3];
+    cv::Mat test[3];
+    for(char i = 0; i < 3; i++) {
+        template_match_res[i].create(
+            displayFrame.cols - templates[i].cols + 1, 
+            displayFrame.rows - templates[i].rows + 1, 
+            CV_32FC1
+        );
+        template_match_res[i].copyTo(test[i]);
+    }
+
+    cv::Mat displayBoxed[3];
 
     // Match templates
     for(char i = 0; i < 3; i++) {
-        cv::Mat match;
         // Perform the template matching
         cv::matchTemplate(displayFrame, templates[i], 
-                          match, CV_TM_SQDIFF);
+                          template_match_res[i], CV_TM_CCOEFF_NORMED);
+        cv::normalize(template_match_res[i], 
+                      template_match_res[i], 
+                      0, 
+                      1, 
+                      cv::NORM_MINMAX);
+
+        cv::matchTemplate(displayFrame, templates[i],
+                          test[i], CV_TM_CCORR);
+        cv::normalize(test[i], 
+                      test[i], 
+                      0, 
+                      1, 
+                      cv::NORM_MINMAX);
+
+        template_match_res[i] += test[i];
+        cv::normalize(template_match_res[i], 
+                      template_match_res[i], 
+                      0, 
+                      1, 
+                      cv::NORM_MINMAX);
 
         double minVal, maxVal;
         cv::Point minLoc, maxLoc;
-        cv::minMaxLoc(match, &minVal, &maxVal, &minLoc, &maxLoc);
+        cv::minMaxLoc(template_match_res[i], 
+                      &minVal, &maxVal, &minLoc, &maxLoc);
 
-        displayFrame.copyTo(template_match_res[i]);
-        cv::rectangle(template_match_res[i], minLoc, 
-                      cv::Point(minLoc.x + templates[i].size().width,
-                                minLoc.y + templates[i].size().height), 
-                      255, 2);
+        displayFrame.copyTo(displayBoxed[i]);
+        cv::rectangle(displayBoxed[i], maxLoc, 
+                      cv::Point(maxLoc.x + templates[i].size().width,
+                                maxLoc.y + templates[i].size().height), 
+                      0, 2);
     }
 
     cv::imshow("Blue 1", templates[0]);
@@ -48,6 +81,10 @@ int main(int argc, char **argv) {
     cv::imshow("Blue 1 res", template_match_res[0]);
     cv::imshow("Blue 2 res", template_match_res[1]);
     cv::imshow("Green res", template_match_res[2]);
+
+    cv::imshow("Blue 1 Box", displayBoxed[0]);
+    cv::imshow("Blue 2 Box", displayBoxed[1]);
+    cv::imshow("Green Box", displayBoxed[2]);
 
     // Display the image
     cv::imshow("Raw", displayFrame);
