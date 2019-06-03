1a2a702eb72bb6839c5dbbfc35a9c5dd5417360f
Nick Hafner <nrh16@ic.ac.uk>
2019-06-03 10:54:50 +0100

    testing esp8266

--- mbed/esp8266_test/esp8266_test.cpp
+++ mbed/esp8266_test/esp8266_test.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
 #include "ATParser.h"
 
+#include <string>
+
 Serial pc(USBTX, USBRX);
 // ESP-01 tx and rx pins
 BufferedSerial *esp_bs;
@@ -10,7 +12,7 @@ ATParser *esp;
 DigitalOut reset(PA_0);
 
 #define REMOTE_IP "10.42.0.1"
-#define REMOTE_PORT 13 /*53821*/
+#define REMOTE_PORT 25565 /*53821*/
 #define DEFAULT_TIMEOUT 50
 
 char ssid[32] = "nickick-laptop";
@@ -23,7 +25,7 @@ bool connect_to_network() {
     esp->setTimeout(8000);
     
     // Try to connect
-    if(!(esp->send("AT+CWJAP=\"%s\",\"%s\"", ssid, pwd) && 
+    if(!(esp->send("AT+CWJAP_CUR=\"%s\",\"%s\"", ssid, pwd) && 
          esp->recv("OK")))
         ret = false;
     
@@ -32,6 +34,8 @@ bool connect_to_network() {
 
     if(ret)
         pc.printf("Successfully connected to \"%s\".\n", ssid);
+    else
+        pc.printf("Failed to connect to \"%s\".\n", ssid);
 
     return ret;
 }
@@ -45,11 +49,12 @@ bool connect_TCP() {
     esp->send("AT+CIPMUX=0") && esp->recv("OK");
 
     // Longer timeout for this
-    esp->setTimeout(8000);
+    esp->setTimeout(1000);
 
     // Make the connection
-    if(!(esp->send("AT+CIPSTART=\"TCP\",\"%s\",%d", REMOTE_IP, REMOTE_PORT)
-         && esp->recv("OK"))) {
+    if((esp->send("AT+CIPSTART=\"TCP\",\"%s\",%d", REMOTE_IP, REMOTE_PORT)
+         && esp->recv("ALREADY CONNECTED"))) {
+        pc.printf("Already Connected\n");
         ret = true;
     }
     else {
@@ -58,8 +63,9 @@ bool connect_TCP() {
         esp->send("AT+CIPCLOSE") && esp->recv("OK");
 
         // Try to connect again
-        if(!(esp->send("AT+CIPSTART=\"TCP\",\"%s\",%d", REMOTE_IP, REMOTE_PORT)
+        if((esp->send("AT+CIPSTART=\"TCP\",\"%s\",%d", REMOTE_IP, REMOTE_PORT)
              && esp->recv("OK"))) {
+            pc.printf("Connection closed and reconnected\n");
             ret = true;
         }
     }
@@ -69,6 +75,8 @@ bool connect_TCP() {
 
     if(ret) 
         pc.printf("Successfully connected to \"%s\".\n", REMOTE_IP);
+    else
+        pc.printf("Failed to connect to \"%s\".\n", REMOTE_IP);
 
     return ret;
 }
@@ -77,31 +85,40 @@ bool check_connection_status() {
     bool ret = false;
 
     esp->send("AT+CIPSTATUS");
-    int status;
+    int status = 0;
     esp->recv("STATUS:%d", &status);
+
+    pc.printf("Status: %d", status);
 
     switch(status) {
     // TCP or UDP connection
     case 3:
-    ret = true;
+        pc.printf("Status ok\n");
+        ret = true;
         break;
     // Connected to network and IP obtained - not connected to TCP
     case 2:
     // TCP or UDP is disconnected
     case 4:
+        pc.printf("TCP disconnected...");
         if(!connect_TCP())
             pc.printf("Lost connection with \"%s\".\n", REMOTE_IP);
-        else
+        else {
+            pc.printf("Reconnected.\n");
             ret = true;
+        }
         break;
     // Not connected to a network
     case 5:
+        pc.printf("Not connected to network...");
         if(!connect_to_network())
             pc.printf("Lost connection with \"%s\".\n", ssid);
         else if(!connect_TCP())
-            pc.printf("Failed to connect to \"%s\".\n", REMOTE_IP);
-        else
+            pc.printf("Connected to network - Failed to connect to \"%s\".\n", REMOTE_IP);
+        else {
+            pc.printf("Connected.");
             ret = true;
+        }
         break;
     }
     
@@ -122,12 +139,12 @@ void setup() {
     reset = 1;
     
     // Wait for crazy stuff to disappear
-    wait(2);
+    wait(3);
     
     // Setup serial and parser
     esp_bs = new BufferedSerial(PA_9, PA_10);
     esp = new ATParser(*esp_bs);
-    
+
     // Set timeout
     esp->setTimeout(DEFAULT_TIMEOUT);
 
@@ -135,19 +152,21 @@ void setup() {
     esp_bs->baud(115200);
 
     // Check that all is ok
-    if(!(esp->send("AT") && esp->recv("OK")))
-        pc.printf("ESP8266 not ok :(");
+    while(!(esp->send("AT") && esp->recv("OK")));
+    pc.printf("ESP8266 ok :)");
     
     // Turn off AT command echoing
     esp->send("ATE0") && esp->recv("OK");
 
-    // Connect to WiFi network
-    if(!connect_to_network())
-        pc.printf("Failed to connect to \"%s\". Soz m8", ssid);
+    connect_to_network();
+    connect_TCP();
 
-    // Connect via TCP
-    if(!connect_TCP()) 
-        pc.printf("Failed to connect to \"%s\". RIP", REMOTE_IP);
+    // Check status
+    while(!check_connection_status());
+
+    // No-longer send AT commands, just send data
+    //esp->send("AT+CIPSEND");
+
 
     pc.printf("\n---------- Setup Complete ----------\n");
 }
@@ -156,16 +175,19 @@ int main() {
     setup();
     char buf[256];
     int len;
+
+    const char response[] = { 0xFF, 0x05, 0x07, 0x00, 0x00, 0x00, 0x00, 0xFE };
     
     // Set the timeout to be very low now as we are looking for many 
     // different messages
     // esp->setTimeout(5);
 
-    Timer status_timer;
-    status_timer.start();
+    Timer send_timer;
+    send_timer.start();
 
-    int status_check_delay_ms = 2000;
+    int send_delay_ms = 100;
 
+    esp->send(response);
     while(true) {
         // Read incoming TCP messages if existant
         if(esp->recv("+IPD,%d:", &len)) {
@@ -173,15 +195,17 @@ int main() {
             int data = esp->read(buf, len);
             pc.printf("Received: %.*s\n", data, buf);
         }
-        
-        // Check status
-        if(status_timer.read_ms() > status_check_delay_ms) {
-            bool connected = check_connection_status();
-            if(connected)
-                status_check_delay_ms = 2000;
-            else
-                status_check_delay_ms = 200;
-            status_timer.reset();
+
+
+        // Send response
+        if(send_timer.read_ms() > send_delay_ms) {
+            esp->send("AT+CIPSEND=%d", sizeof(response)/sizeof(char));
+            if(esp->recv(">")) {
+                // Can't use send here as null characters end the string
+                esp->write(response, sizeof(response)/sizeof(char));
+                pc.printf("Sending message...");
+            }
+            send_timer.reset();
         }
     }
 }
